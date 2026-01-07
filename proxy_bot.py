762d9a9e269385891030e83ec3f8a0f6e6c7b930
rakibkumar151 <rakibkumar151@gmail.com>
2026-01-08 04:22:02 +0530

    Update proxy_bot.py

--- proxy_bot.py
+++ proxy_bot.py
@@ -5,45 +5,29 @@ import time
 # ৬০+ হাই-কোয়ালিটি লাইভ সোর্স (যেগুলো প্রতি ৫-১০ মিনিটে আপডেট হয়)
 SOURCES = [
     # TheSpeedX (High Traffic)
-    "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt",
     "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt",
     "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt",
     # Monosans (Very Clean)
-    "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt",
     "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/socks4.txt",
     "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/socks5.txt",
     # Vakhov (Freshly Scraped)
-    "https://raw.githubusercontent.com/vakhov/fresh-proxy-list/master/http.txt",
     "https://raw.githubusercontent.com/vakhov/fresh-proxy-list/master/socks4.txt",
     "https://raw.githubusercontent.com/vakhov/fresh-proxy-list/master/socks5.txt",
     # Jetkai (Validated Daily)
-    "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-http.txt",
     "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-socks4.txt",
     "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-socks5.txt",
     # MuRongPIG
-    "https://raw.githubusercontent.com/MuRongPIG/Proxy-Master/main/http.txt",
     "https://raw.githubusercontent.com/MuRongPIG/Proxy-Master/main/socks4.txt",
     "https://raw.githubusercontent.com/MuRongPIG/Proxy-Master/main/socks5.txt",
     # ShiftyTR
-    "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/http.txt",
     "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks4.txt",
-    "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks5.txt",
-    # ProxyScrape API
-    "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all",
-    "https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks4&timeout=10000&country=all",
-    "https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks5&timeout=10000&country=all",
+    "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks5.txt"
     # Others (Uptimer, Fate0, etc)
     "https://raw.githubusercontent.com/proxifly/free-proxy-list/main/proxies/all/data.txt",
     "https://raw.githubusercontent.com/roosterkid/openproxylist/main/HTTPS_RAW.txt",
     "https://raw.githubusercontent.com/sunny9577/proxy-scraper/master/proxies.txt",
-    "https://raw.githubusercontent.com/mmpx12/proxy-list/master/http.txt",
-    "https://raw.githubusercontent.com/UptimerBot/proxy-list/main/proxies/http.txt",
     "https://raw.githubusercontent.com/rdavydov/proxy-list/main/proxies/socks5.txt",
-    "https://raw.githubusercontent.com/officialputuid/Proxy-List/master/http.txt",
-    "https://raw.githubusercontent.com/Z4NGLU89/proxy-list/main/http.txt",
-    "https://raw.githubusercontent.com/andrei60/proxy-list/main/http.txt",
     "https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt",
-    "https://raw.githubusercontent.com/ErcinDedeoglu/proxies/main/proxies/http.txt",
     "https://raw.githubusercontent.com/ErcinDedeoglu/proxies/main/proxies/socks5.txt"
 ]
 
@@ -90,3 +74,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
